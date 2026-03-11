be40fdafcb5d80224f9e60d5133ecd3e623e0d2e
Fernando <nando.nas@gmail.com>
2026-03-11 20:38:24 -0300

    Base App.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import { APP_ROUTES } from './app.routes';
 import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
@@ -9,5 +9,9 @@ import en from '@angular/common/locales/en';
 registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideNzI18n(en_US)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(APP_ROUTES),
+    provideNzI18n(en_US)
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
+export const APP_ROUTES: Routes = [
     {
         path: '',
-        loadChildren: () => import('./app/feature/home/home.routes').then(m => m.HOME_ROUTES)
+        loadChildren: () => import('./feature/home/home.routes').then(m => m.HOME_ROUTES)
     }
 ];
