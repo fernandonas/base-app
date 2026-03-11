26f4201d5e69cd5e8044b41dc95c3d3065c82178
Fernando <nando.nas@gmail.com>
2026-03-11 20:15:44 -0300

    Added home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () => import('./feature/home/home').then(m => m.Home) 
+        loadChildren: () => import('./app/feature/home/home.routes').then(m => m.HOME_ROUTES)
     }
 ];
