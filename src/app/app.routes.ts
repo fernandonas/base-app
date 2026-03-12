2224a08c035a4160a696e5e9863d03186ec771f2
Fernando <nando.nas@gmail.com>
2026-03-11 21:51:20 -0300

    Added login page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const APP_ROUTES: Routes = [
     {
         path: '',
         loadChildren: () => import('./feature/home/home.routes').then(m => m.HOME_ROUTES)
+    },
+    {
+        path: 'login',
+        loadChildren: () => import('./feature/login/login.routes').then(m => m.LOGIN_ROUTES)
     }
 ];
---
+++ src/app/feature/login/login.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+
+export const LOGIN_ROUTES: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./pages/login/login').then(m => m.Login) 
+    }
+];
